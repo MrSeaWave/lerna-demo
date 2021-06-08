1a848058dc11a01796039c040b7c51c3fa952c12
MrSeaWave <774866545@qq.com>
2021-06-08 11:44:10 +0800

    chore: 配置

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js'], // 指定要测试的文件类型的后缀
-  testMatch: ['<rootDir>/**/*.test.js'],
+  testMatch: ['<rootDir>/**/*.test.js']
 };
