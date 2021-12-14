edfbdd6de9ca03973a49201faac3c5d5f0aa5054
MrSeaWave <774866545@qq.com>
2021-12-14 22:19:35 +0800

    feat: tyest

--- packages/pkg-c/src/index.js
+++ packages/pkg-c/src/index.js
@@ -1,7 +1,7 @@
 import chalk from 'chalk';
 
 function sum (a, b) {
-  console.log(chalk.green('123 go sum params:'), a, b);
+  console.log(chalk.green('123 go --> sum params:'), a, b);
   return a + b;
 }
 
